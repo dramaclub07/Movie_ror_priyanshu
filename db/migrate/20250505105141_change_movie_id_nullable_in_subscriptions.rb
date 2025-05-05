593d191c1e9e34a01e82a85ba7d7ae26e63b959b
Dramaclub07 <Priyanshu7ronaldo@gmail.com>
2025-05-05 17:35:53 +0530

    [Priyanshu] ADD: Migration for null movie in subscription

--- db/migrate/20250505105141_change_movie_id_nullable_in_subscriptions.rb
+++ db/migrate/20250505105141_change_movie_id_nullable_in_subscriptions.rb
@@ -1,3 +1,5 @@
-def change
-  change_column_null :subscriptions, :movie_id, true
+class ChangeMovieIdNullableInSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :subscriptions, :movie_id, true
+  end
 end
