e1c4b0b85ebb5d26caa915eafcd73c22e4158fb3
Priyanshu Singh <priyanshu.singh@magicedtech.com>
2025-05-16 14:30:54 +0530

    [Priyanshu] MOD: Seeded Movie data

--- db/migrate/20250516075722_add_role_to_admin_users.rb
+++ db/migrate/20250516075722_add_role_to_admin_users.rb
@@ -1,5 +1,7 @@
 class AddRoleToAdminUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :admin_users, :role, :string, default: "admin"
+    unless column_exists?(:admin_users, :role)
+      add_column :admin_users, :role, :string, default: "admin"
+    end
   end
 end
